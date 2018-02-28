f6b7202a0f3d8628caa432e70ac391bad62787fc
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-28 15:55:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(arr)
+  arr.sort {|a, b| a <=>b}
+end
 
+def sort_array_desc(arr)
+  arr.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(arr)
+  arr.sort {|a, b| a.size <=> b.size}
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  new_arr = []
+  arr.each do |element|
+    element[2] =  '$'
+    new_arr.push(element)
+  end
+  new_arr
+end
